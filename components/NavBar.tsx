9cdced367dfcf51cf1bf06a333735db5cb5a3f5d
DavidArtifacts <davidrodr95@gmail.com>
2025-02-14 15:49:40 -0600

    fix: update NavBar component to enhance active link styling for improved navigation clarity

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,16 +8,22 @@ export default function NavBar({ user }: { user: User }) {
   const path = usePathname();
   const adminPattern = /^\/admin\/?$/;
   const homePattern = /^\/$/;
-  const profilePattern = /^\/profile\/?$/;
+  const profilePattern = /^\/profile\/[a-zA-Z0-9_-]+\/?$/;
   const boardPattern = /^\/boards(\/[a-zA-Z0-9_-]+)?\/?$/;
   const notePattern = /^\/boards\/[a-zA-Z0-9_-]+\/notes(\/[a-zA-Z0-9_-]+)?\/?$/;
 
   return (
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className={`nav-item ${homePattern.test(path) ? 'active' : ''}`}>
+        <li
+          className={`nav-item ${
+            homePattern.test(path) ? 'active text-primary' : ''
+          }`}
+        >
           <a
-            className={`nav-link ${homePattern.test(path) ? 'active' : ''}`}
+            className={`nav-link ${
+              homePattern.test(path) ? 'active text-primary' : ''
+            }`}
             href="/"
           >
             <span className="nav-link-icon d-md-none d-lg-inline-block">
@@ -31,7 +37,9 @@ export default function NavBar({ user }: { user: User }) {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-home"
+                className={`icon icon-tabler ${
+                  homePattern.test(path) ? 'text-primary' : ''
+                } icons-tabler-outline icon-tabler-home`}
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
@@ -44,12 +52,16 @@ export default function NavBar({ user }: { user: User }) {
         </li>
         <li
           className={`nav-item ${
-            boardPattern.test(path) || notePattern.test(path) ? 'active' : ''
+            boardPattern.test(path) || notePattern.test(path)
+              ? 'active text-primary'
+              : ''
           }`}
         >
           <a
             className={`nav-link ${
-              boardPattern.test(path) || notePattern.test(path) ? 'active' : ''
+              boardPattern.test(path) || notePattern.test(path)
+                ? 'active text-primary'
+                : ''
             }`}
             href="/boards"
           >
@@ -64,7 +76,11 @@ export default function NavBar({ user }: { user: User }) {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-layout-dashboard"
+                className={`icon icon-tabler ${
+                  boardPattern.test(path) || notePattern.test(path)
+                    ? 'text-primary'
+                    : ''
+                } icons-tabler-outline icon-tabler-layout-dashboard`}
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M5 4h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
@@ -76,9 +92,15 @@ export default function NavBar({ user }: { user: User }) {
             <span className="nav-link-title">Boards</span>
           </a>
         </li>
-        <li className={`nav-item ${profilePattern.test(path) ? 'active' : ''}`}>
+        <li
+          className={`nav-item ${
+            profilePattern.test(path) ? 'active text-primary' : ''
+          }`}
+        >
           <a
-            className={`nav-link ${profilePattern.test(path) ? 'active' : ''}`}
+            className={`nav-link ${
+              profilePattern.test(path) ? 'active text-primary' : ''
+            }`}
             href={`/profile/${user?.id}`}
           >
             <span className="nav-link-icon d-md-none d-lg-inline-block">
@@ -92,7 +114,9 @@ export default function NavBar({ user }: { user: User }) {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-user"
+                className={`icon icon-tabler ${
+                  profilePattern.test(path) ? 'text-primary' : ''
+                } icons-tabler-outline icon-tabler-user`}
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
@@ -105,9 +129,15 @@ export default function NavBar({ user }: { user: User }) {
       </ul>
       {user?.role === 'admin' && (
         <ul className="navbar-nav mb-2 mb-lg-0">
-          <li className={`nav-item ${adminPattern.test(path) ? 'active' : ''}`}>
+          <li
+            className={`nav-item ${
+              adminPattern.test(path) ? 'active text-primary' : ''
+            }`}
+          >
             <a
-              className={`nav-link ${adminPattern.test(path) ? 'active' : ''}`}
+              className={`nav-link ${
+                adminPattern.test(path) ? 'active text-primary' : ''
+              }`}
               href="/admin"
             >
               <span className="nav-link-icon d-md-none d-lg-inline-block">
@@ -121,7 +151,9 @@ export default function NavBar({ user }: { user: User }) {
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="icon icon-tabler icons-tabler-outline icon-tabler-user-shield"
+                  className={`icon icon-tabler ${
+                    adminPattern.test(path) ? 'text-primary' : ''
+                  } icons-tabler-outline icon-tabler-user-shield`}
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M6 21v-2a4 4 0 0 1 4 -4h2" />
