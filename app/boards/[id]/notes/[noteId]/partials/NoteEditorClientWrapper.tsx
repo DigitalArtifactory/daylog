ce21e198be556c6fb7b190a15d108abed8843616
DavidArtifacts <davidrodr95@gmail.com>
2025-02-17 11:41:07 -0600

    fix: remove unnecessary router push after note update in NoteEditorClientWrapper

--- app/boards/[id]/notes/[noteId]/partials/NoteEditorClientWrapper.tsx
+++ app/boards/[id]/notes/[noteId]/partials/NoteEditorClientWrapper.tsx
@@ -35,7 +35,6 @@ export default function NoteEditorClientWrapper({
     if (!note) return;
     note.content = content;
     if (note) await updateNote(note);
-    router.push('?notify=saved');
   };
 
   return (
