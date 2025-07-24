ec25268b22332aaa1e0a61130659dcffae61306a
David R. <davidrodr95@gmail.com>
2025-07-24 00:04:34 -0600

    refactor: EditorPlaceholder.test.tsx

--- app/boards/[id]/notes/[noteId]/components/EditorPlaceholder.test.tsx
+++ app/boards/[id]/notes/[noteId]/components/EditorPlaceholder.test.tsx
@@ -2,37 +2,37 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import NoteEditorPlaceholder from './EditorPlaceholder';
 
+function mockNote(
+  element: Element | null,
+  className: string,
+  colClassName: string
+): boolean {
+  if (!element) return false;
+  return (
+    element.classList.contains(className) &&
+    element.classList.contains(colClassName)
+  );
+}
+
 describe('NoteEditorPlaceholder', () => {
   it('renders the placeholder elements correctly', () => {
     render(<NoteEditorPlaceholder />);
 
     // Check for the presence of placeholder elements
     expect(
-      screen.getByText((_, element) => {
-        if (!element) return false;
-        return (
-          element.classList.contains('placeholder') &&
-          element.classList.contains('col-9')
-        );
-      })
+      screen.getByText((_, element) =>
+        mockNote(element, 'placeholder', 'col-9')
+      )
     ).toBeInTheDocument();
     expect(
-      screen.getByText((_, element) => {
-        if (!element) return false;
-        return (
-          element.classList.contains('placeholder-xs') &&
-          element.classList.contains('col-10')
-        );
-      })
+      screen.getByText((_, element) =>
+        mockNote(element, 'placeholder-xs', 'col-10')
+      )
     ).toBeInTheDocument();
     expect(
-      screen.getByText((_, element) => {
-        if (!element) return false;
-        return (
-          element.classList.contains('placeholder-xs') &&
-          element.classList.contains('col-11')
-        );
-      })
+      screen.getByText((_, element) =>
+        mockNote(element, 'placeholder-xs', 'col-11')
+      )
     ).toBeInTheDocument();
   });
 });
